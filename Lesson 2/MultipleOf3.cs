4d960f9f4151d43fcc2d33b5723dc0b3c3dbfc95
BraveSpiritt <davidsoghomonyan.htspc@gmail.com>
2024-04-08 20:25:22 +0400

    loops

---
+++ Lesson 2/MultipleOf3.cs
@@ -0,0 +1,13 @@
+﻿int index = 1;
+int counter = 0;
+while (index < 101)
+{
+    if (index % 3 == 0)
+    {
+
+        counter += index;
+    }
+    index++;
+}
+
+Console.WriteLine(counter);
---
+++ Lesson 2/Program.cs
@@ -0,0 +1,9 @@
+﻿int index = 0;
+int counter = 0;
+while (index < 101)
+{
+    counter += index;
+    index++;
+}
+
+Console.WriteLine(counter);
