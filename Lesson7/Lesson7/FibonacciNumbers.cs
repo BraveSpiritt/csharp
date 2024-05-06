c778206ff8d884c59cb34c499fa716df335cedc4
BraveSpiritt <davidsoghomonyan.htspc@gmail.com>
2024-05-06 22:15:15 +0400

    half-part of tasks done !

---
+++ Lesson7/Lesson7/FibonacciNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Fibonacci
+{
+    public int CalculateFibonacci(int endingPoint) {
+        if(endingPoint <= 1)
+        {
+            return endingPoint;
+        }
+        return CalculateFibonacci(endingPoint - 1) + CalculateFibonacci(endingPoint - 2);
+    }
+
+    public static void Main(string[] args)
+    {
+        int sum = new BeerAndBottles().CalculateAmountOfBeers(10);
+        Console.WriteLine(sum);
+    }
+
+}
